2cc71b6d09c8493464ecc1c49b75ae0ae34b4a7a
DewaldSchoonderwoerd <dewald.schoonderwoerd@gmail.com>
2026-03-17 02:00:46 +0100

    Adding the endpoint paths

---
+++ src/main/java/com/surepay/qa/support/constants/ApiPaths.java
@@ -0,0 +1,10 @@
+package com.surepay.qa.support.constants;
+
+public final class ApiPaths {
+    public static final String USERS = "/users";
+    public static final String POSTS = "/posts";
+    public static final String COMMENTS = "/comments";
+
+    private ApiPaths() {
+    }
+}
